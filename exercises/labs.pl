5edb8430c25dcfe152197846390e66b6029f9969
Eric Yip <yip.e@northeastern.edu>
2023-11-21 16:47:06 -0500

    Completed labs.pl and resolution.pl exercises

--- exercises/labs.pl
+++ exercises/labs.pl
@@ -4,3 +4,5 @@ is_lab(shillman).
 takes(turing, cs307).
 takes(hopper, cs307).
 takes(alberuni, cs220).
+
+takes_lab(S) :- takes(S, L), meets_in(L, R), is_lab(R).
--- exercises/resolution.pl
+++ exercises/resolution.pl
@@ -2,4 +2,5 @@ takes(jane_doe, his201).
 takes(jane_doe, cs254).
 takes(ajit_chandra, art302).
 takes(ajit_chandra, cs254).
-classmates(X, Y) :- takes(X, Z), takes(Y, Z).
+different_student(jane_doe, ajit_chandra).
+classmates(X, Y) :- takes(X, Z), takes(Y, Z), different_student(X, Y).
